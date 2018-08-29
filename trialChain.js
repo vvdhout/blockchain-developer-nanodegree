f3516422541965414cdca99ed7df6ca44160d3d0
vvdhout <valentijnvdh@gmail.com>
2018-08-29 21:48:28 +0200

    =Updated and streamlined private blockchain project

---
+++ trialChain.js
@@ -0,0 +1,159 @@
+/* This is a test version. Just trying to get the concept of writing a private blockchain accurate. */
+
+// First we will import the needed modules
+
+const SHA256 = require('crypto-js/sha256');
+const level = require('level')
+const chaindb = './trialchaindb';
+const db = level(chaindb, {valueEncoding: 'json'})
+
+
+var getChainLength = new Promise((resolve, reject) => {			
+	var blockCount = 0;
+	var stream = db.createReadStream();
+	stream.on('data', function(block) {
+		if(Object.keys(block).length > 0) {
+			blockCount ++;
+		}
+	})
+	stream.on('close', function() {
+		resolve(blockCount);
+	})
+});
+
+
+
+class Blockchain {
+	constructor() {
+		let genesisPromise = new Promise((resolve,reject) => { 
+			getChainLength.then((blockCount) => {
+				if(blockCount === 0) {
+					let genesisBlock = {
+						time: 0,
+						height: 0,
+						previousBlockHash: '',
+						data: 'This is the genesis block of the chain',
+						hash: ''
+					}
+					genesisBlock.hash = SHA256(JSON.stringify(genesisBlock)).toString();
+					db.put(0, genesisBlock, function(err) {
+						resolve('We succesffully created the blockchain and added a genesis block.')
+					})
+				}
+				else {
+					resolve('The blockchain already exists with a genesis block in it.')
+				}
+			});
+		});
+		genesisPromise.then((message) => {
+			console.log(message);
+		})
+	}
+
+	addBlock(newBlock) {
+		let createBlockPromise = new Promise((resolve,reject) => {
+			getChainLength.then((blockCount) => {
+				let chainHeight = blockCount - 1;
+				db.get(chainHeight, function(err, value) {
+					if (err) { 
+						resolve('Could not get the block because we received an error.')
+					}
+					else { 
+						resolve(value)
+					}
+				})
+			})
+		})
+		createBlockPromise.then((value) => {
+			newBlock.time = new Date().getTime().toString().slice(0,-3);
+			newBlock.height = value.height + 1
+			newBlock.previousBlockHash = value.hash;
+			newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+			db.put(newBlock.height, newBlock, function(err) {
+				console.log(`We added the block to the blockchain.`)
+			})
+		})
+	}
+
+	getHeight() {
+		getChainLength.then((blockCount) => {
+			let chainHeight = blockCount - 1;
+			console.log(chainHeight);
+		})
+	}
+
+	getBlock(height) {
+		let getBlockPromise = new Promise((resolve,reject) => { 
+			db.get(height, function(err, value) {
+				if(err) {
+					console.log('We were unable to get the block due to some error.')
+				}
+				else {
+					resolve(value);
+				}
+			});
+		});
+		getBlockPromise.then((value) => {
+			console.log(value);
+			return value
+		})
+	}
+
+	validateBlock(height){
+		db.get(height, function(err, value) {
+			let blockHash = value.hash;
+			value.hash = '';
+			let validHash = SHA256(JSON.stringify(value)).toString();
+			if(validHash === blockHash) {
+				console.log(`Block #${value.height} is valid.`)
+				return true;
+			}
+			else {
+				console.log(`WARNING: Block #${value.height} is not valid!`);
+				return false
+			}
+		})
+	}
+
+	validateChain() {
+
+	}
+
+
+}
+
+class Block {
+	constructor(data) {
+		this.previousBlockHash = '';
+		this.time = 0;
+		this.height = 0;
+		this.body = data;
+		this.hash = '';
+    } 
+}
+
+let firstChain = new Blockchain();
+
+// // 1. =========== Adding a new block to the blockchain.
+// firstChain.addBlock(new Block('And we are adding some more data onto the blockchain.'));
+
+// // 2. =========== Console.log the chain of blocks
+// var stream = db.createReadStream();
+// let chain = [];
+// 	stream.on('data', function(block) {
+// 		chain.push(block);
+// 	});
+// 	stream.on('close', function() {
+// 		console.log(chain);
+// })
+
+// // 3. =========== Get height
+// firstChain.getHeight();
+
+// // 4. =========== Get block object
+// firstChain.getBlock(2);
+
+// // 5. =========== Get block object
+// firstChain.validateBlock(2); 
+
+// firstChain.validateChain();
